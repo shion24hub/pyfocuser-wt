bbacd27f04d76037ce5f56dabbffdcc9d7b00d10
Shion Noguchi <shion24stat@gmail.com>
2023-01-10 22:43:36 +0900

    rename lib->libs

---
+++ focuser-wt/libs/tweet.py
@@ -0,0 +1,25 @@
+import tweepy
+import datetime
+
+from lib.config import (
+    API_KEY,
+    API_SECRET,
+    ACCESS_TOKEN,
+    ACCESS_TOKEN_SECRET,
+    BEARER_TOKEN,
+)
+
+def ClientInfo() :
+    client = tweepy.Client(
+        bearer_token=BEARER_TOKEN,
+        consumer_key=API_KEY,
+        consumer_secret=API_SECRET,
+        access_token=ACCESS_TOKEN,
+        access_token_secret=ACCESS_TOKEN_SECRET,
+    )
+    return client
+
+def createTweet(message) :
+    now = datetime.datetime.now()
+    tweet = ClientInfo().create_tweet(text=message)
+    return tweet
